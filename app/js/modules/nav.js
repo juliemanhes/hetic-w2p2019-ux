c44dcdf0ffc0fe4eabaa7502b31105154793d3eb
SIMAM77 <souleimane.imam@hetic.net>
2018-04-13 15:16:20 +0200

    js nav text-color

--- app/js/modules/nav.js
+++ app/js/modules/nav.js
@@ -1,20 +1,6 @@
 var navItem = document.querySelectorAll(".nav--wrap-item");
-// console.log(navItem);
 var toto = document.querySelector("body");
-
-var i = 0;
-
-var intElemScrollTop = window.scrollY;
-// console.log(intElemScrollTop);
-// console.log(toto);
-
-// navItem[i].style.fontSize = "2rem";
 var scrollTotal = toto.scrollHeight;
-
-function tata(event) {
-  console.log(event);
-}
-
 var i = 0;
 var pourcentageY = 0;
 
@@ -25,6 +11,9 @@ window.addEventListener("scroll", function() {
   if (pourcentageY == 10 || pourcentageY == 19) {
     i = 0;
     navItem[i].style.fontSize = "40px";
+    for (var y = 0; y < navItem.length; y++) {
+      navItem[y].style.color = "white";
+    }
   } else if (pourcentageY > 20) {
     navItem[0].style.fontSize = "20px";
   }
@@ -32,14 +21,18 @@ window.addEventListener("scroll", function() {
   if (pourcentageY == 20 || pourcentageY == 29) {
     i = 1;
     navItem[i].style.fontSize = "40px";
+    for (var y = 0; y < navItem.length; y++) {
+      navItem[y].style.color = "purple";
+    }
   } else if (20 > pourcentageY || pourcentageY > 30) {
     navItem[1].style.fontSize = "20px";
   }
 
   if (pourcentageY == 30 || pourcentageY == 39) {
     i = 2;
-    console.log(i);
-
+    for (var y = 0; y < navItem.length; y++) {
+      navItem[y].style.color = "white";
+    }
     navItem[i].style.fontSize = "40px";
   } else if (30 > pourcentageY || pourcentageY > 40) {
     navItem[2].style.fontSize = "20px";
@@ -47,38 +40,11 @@ window.addEventListener("scroll", function() {
 
   if (pourcentageY == 40) {
     i = 3;
-    console.log(i);
+    for (var y = 0; y < navItem.length; y++) {
+      navItem[y].style.color = "purple";
+    }
     navItem[i].style.fontSize = "40px";
   } else if (pourcentageY < 40) {
     navItem[3].style.fontSize = "20px";
   }
 });
-
-// function navigation() {
-//   if (pourcentageY < 10) {
-//     i = 0;
-//     console.log(i);
-//     navItem[i].style.fontSize = "40px";
-//   }
-
-//   if (10 < pourcentageY < 20) {
-//     i = 1;
-//     console.log(i);
-
-//     navItem[i].style.fontSize = "40px";
-//   }
-
-//   if (20 < pourcentageY < 30) {
-//     i = 2;
-//     console.log(i);
-
-//     navItem[i].style.fontSize = "3rem";
-//   }
-
-//   if (40 < pourcentageY) {
-//     i = 3;
-//     console.log(i);
-
-//     navItem[i].style.fontSize = "3rem";
-//   }
-// }
