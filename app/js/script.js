d17b18b22561be44618c1dd983c6fc3ffaa23ef5
Anne Lagarde <anne.lagarde@hetic.net>
2018-04-13 17:50:55 +0200

    Update script.js

--- app/js/script.js
+++ app/js/script.js
@@ -36,29 +36,14 @@ function type1() {
 }
 
 function type2() {
-    var j = 0;
-    var txt2 = 'On ne se connaît pas encore, mais ça ne saurait tarder...';
-    var speed = 50;
-    
-     var callType = function(){
-        if (window.scrollY >= 935) {
-            typeWriter();
-            window.removeEventListener('scroll', callType);
-        }
-     }
-    
-    var bot = document.querySelectorAll('.ia-dialog2'); 
-    for (let i = 0; i < bot.length; i++){
-        let parent = bot[i].parentNode;
-        window.addEventListener('scroll', callType)
-    }
-    
-    function typeWriter() {
-      if (j < txt2.length) {
-        document.querySelector(".ia-dialog2--content").innerHTML += txt2.charAt(j);
-        j++;
-        setTimeout(typeWriter, speed);
-      }
+  var j = 0;
+  var txt2 = "On ne se connait pas encore, mais ça ne saurait tarder...";
+  var speed = 50;
+
+  var callType = function() {
+    if (window.scrollY >= 935) {
+      typeWriter();
+      window.removeEventListener("scroll", callType);
     }
   };
 }
@@ -75,23 +60,4 @@ function typeWriter() {
     j++;
     setTimeout(typeWriter, speed);
   }
-}
-
-// BURGER NAV //
-
-var burger = document.querySelectorAll('.header--nav-burger div');
-var header = document.querySelector('.header--buttons');
-var burger1 = document.querySelector('.burger-1');
-var burger2 = document.querySelector('.burger-2');
-var burger3 = document.querySelector('.burger-3');
-
-if (burger) {
-    for (let i = 0; i < burger.length;i++) {
-        burger[i].addEventListener('click', function() {
-            header.classList.toggle('hidden');
-            burger1.classList.toggle('burger-transform-1');
-            burger2.classList.toggle('burger-transform-3');
-            burger3.classList.toggle('burger-transform-2');
-        });
-    }
 }
